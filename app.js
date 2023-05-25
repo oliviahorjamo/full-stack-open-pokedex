e07ea99ba68dbec94e3d01020e799275255f5c34
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-05-25 23:12:26 +0300

    Fix script healthcheck

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
 
 app.get('/health', (req, res) => {
